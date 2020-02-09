40a4130187578fca4c1b096d4a2bc857ffaecf60
FabioCampello <fabiocampello@outlook.com.br>
2020-02-09 17:55:51 -0300

    Aula09 - Operadores de Atribuição.

---
+++ src/devdojo/aula09/OperadoresDeAtribuicao.java
@@ -0,0 +1,20 @@
+package devdojo.aula09;
+
+public class OperadoresDeAtribuicao {
+
+	// =, -=, +=, *=, /=, %=	
+	public static void main(String[] args) {
+		
+		float salario = 1000f;
+		salario = salario + 1000f;
+		
+		float salarioNovo = 1000f;
+		salarioNovo += 1500f;
+		
+		System.out.println(salario);
+		System.out.println(salarioNovo);
+		System.out.println("Salário com 10%: " + (salarioNovo * 1.1));
+		
+	}
+	
+}
