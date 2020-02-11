22f0e8d7f32604c3407900cf4d8ebff03fc591a3
FabioCampello <fabiocampello@outlook.com.br>
2020-02-11 20:46:51 -0300

    Aula13 - Controle de fluxo switch e exercícios.

---
+++ src/devdojo/aula13/ControleFluxo3.java
@@ -0,0 +1,42 @@
+package devdojo.aula13;
+
+public class ControleFluxo3 {
+
+	public static void main(String[] args) {
+		// char, int, byte, enum e String
+		byte dia = 5;
+		String semana = "";
+
+		switch (dia) {
+			case 1: {
+				semana = "Domingo";
+				break;
+			}
+			case 2: {
+				semana = "Segunda";
+				break;
+			}
+			case 3: {
+				semana = "Terça";
+				break;
+			}
+			case 4: {
+				semana = "Quarta";
+				break;
+			}
+			case 5: {
+				semana = "Quinta";
+				break;
+			}
+			case 6: {
+				semana = "Sexta";
+				break;
+			}
+			default: {
+				break;
+			}
+		}
+		System.out.println(semana);
+	}
+
+}
