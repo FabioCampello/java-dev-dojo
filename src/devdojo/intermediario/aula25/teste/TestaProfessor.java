1ac42be46a5759eb1057ff74bf455219387a5821
FabioCampello <fabiocampello@outlook.com.br>
2020-02-15 01:28:15 -0300

    Aula26 - Solução do exercício introdução a classes.

--- src/devdojo/intermediario/aula25/teste/TestaProfessor.java
+++ src/devdojo/intermediario/aula25/teste/TestaProfessor.java
@@ -12,7 +12,14 @@ public class TestaProfessor {
 		professor1.setRg("9576423");
 		professor1.setCpf("654852483");
 		
+		Professor professor2 = new Professor();
+		professor2.setNome("Prof. Pardal");
+		professor2.setMatricula("T5364");
+		professor2.setRg("68124587523");
+		professor2.setCpf("854210357");
+		
 		System.out.println(professor1.toString());
+		System.out.println(professor2.toString());
 		
 	}
 	
