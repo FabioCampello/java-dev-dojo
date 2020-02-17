d850459a4bc83a9b248bf7d19fb9020dd7cfec97
FabioCampello <fabiocampello@outlook.com.br>
2020-02-16 22:46:15 -0300

    Aula27 - Introdução a métodos.

---
+++ src/devdojo/intermediario/aula27/TestaCalculadora.java
@@ -0,0 +1,11 @@
+package devdojo.intermediario.aula27;
+
+public class TestaCalculadora {
+
+	public static void main(String[] args) {
+		Calculadora c = new Calculadora();
+		c.somaDoisNumeros();
+		c.subtraiDoisNumeros();
+	}
+	
+}
