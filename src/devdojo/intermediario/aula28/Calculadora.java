d5525656fd4c01257050be69d37988929b29d820
FabioCampello <fabiocampello@outlook.com.br>
2020-02-18 02:06:12 -0300

    Aula28 - Métodos com parâmetros.

---
+++ src/devdojo/intermediario/aula28/Calculadora.java
@@ -0,0 +1,17 @@
+package devdojo.intermediario.aula28;
+
+public class Calculadora {
+	
+	public void somaDoisNumeros(int num1, int num2) {
+		System.out.println(num1 + num2);
+	}
+	
+	public void subtraiDoisNumeros(int num1, int num2) {
+		System.out.println(num1 - num2);
+	}
+	
+	public void multiplicaDoisNumeros(int num1, int num2) {
+		System.out.println(num1 * num2);
+	}
+
+}
---
+++ src/devdojo/intermediario/aula28/TestaCalculadora.java
@@ -0,0 +1,13 @@
+package devdojo.intermediario.aula28;
+
+public class TestaCalculadora {
+
+	public static void main(String[] args) {
+		
+		Calculadora c = new Calculadora();
+		c.somaDoisNumeros(2, 13);
+		c.subtraiDoisNumeros(13, 3);
+		c.multiplicaDoisNumeros(3, 2);
+	}
+	
+}
